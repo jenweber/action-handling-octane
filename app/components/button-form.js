56b37564f88c564deb2cb5242f83fb2b375a9cf7
Jen Weber <j@jenweber.me>
2019-09-15 18:20:33 -0500

    remove components

--- app/components/button-form.js
+++
@@ -1,9 +0,0 @@
-import Component from '@glimmer/component';
-import { action } from '@ember/object';
-
-export default class ButtonFormComponent extends Component {
-  @action
-  localAction() {
-    this.args.addOne(1);
-  }
-}
